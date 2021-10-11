c597187b0795a751ae735c757c4319189ae21d68
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-10-11 13:23:45 -0500

    adding useState and ability to specify limit and start.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 // Import useState and useEffect hooks from React
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // Import the API category from AWS Amplify
-import { API } from 'aws-amplify';
-import './App.css';
+import { API } from "aws-amplify";
+import "./App.css";
 
 const App = () => {
   // Create coins variable and set to empty array
   const [coins, updateCoins] = useState([]);
-  // Define function to all API
-  const fetchCoins = async() => {
-  const data = await API.get('cryptoapi', '/coins')
-  updateCoins(data.coins)
+
+  // Create additional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
+  // Create a new function to allow users to update the input values
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value });
   }
+  // Define function to all API
+  const fetchCoins = async () => {
+    const { limit, start } = input;
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
+    updateCoins(data.coins);
+  };
+
   // Call fetchCoins function when component loads
   useEffect(() => {
-  fetchCoins()
-  }, [])
+    fetchCoins();
+  }, []);
   return (
-  <div className="App">
-  {
-  coins.map((coin, index) => (
-  <div key={index}>
-  <h2>{coin.name} - {coin.symbol}</h2>
-  <h5>${coin.price_usd}</h5>
-  </div>
-  ))
-  }
-  </div>
+    <div className="App">
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>
+            {coin.name} - {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
+    </div>
   );
-  }
-  export default App
+};
+export default App;
