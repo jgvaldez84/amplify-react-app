edb63c9ab691d272f99ed57885f33259a034e7bb
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-10-23 18:00:30 -0500

    moved build folder to docs

---
+++ docs/precache-manifest.899b4ee25a9c8d32642e33a2b216d914.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba42d07e2f958f2c38f1cc801242e93b",
+    "url": "/amplify-react-app/index.html"
+  },
+  {
+    "revision": "c5e7356c255a453d74cf",
+    "url": "/amplify-react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2d5e69bcc5ce1d815567",
+    "url": "/amplify-react-app/static/js/2.a5af855a.chunk.js"
+  },
+  {
+    "revision": "275bb3b8137a52423b99c8b444cc7bc6",
+    "url": "/amplify-react-app/static/js/2.a5af855a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "767e2d359923a94c1e79",
+    "url": "/amplify-react-app/static/js/3.f21fe095.chunk.js"
+  },
+  {
+    "revision": "c5e7356c255a453d74cf",
+    "url": "/amplify-react-app/static/js/main.7303deb6.chunk.js"
+  },
+  {
+    "revision": "20999f2e267646ac776a",
+    "url": "/amplify-react-app/static/js/runtime-main.54b78c69.js"
+  }
+]);
